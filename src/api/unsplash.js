be9a4b87d3adc08c3b90f6ac26f1899d83bc779e
e-barr <31676765+e-barr@users.noreply.github.com>
2019-01-29 20:32:56 -0600

    update

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const unsplashApi = `${process.env.REACT_APP_UNSPLASH_API}`
+
 export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        Authorization: `Client-ID 036fb9d0e27f7705c9e931c9246ca7254610bdd16d6ff470cfd9d82d1759b5ad`
+        Authorization: `Client-ID ${unsplashApi}`
     }
 })
