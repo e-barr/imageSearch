4026a73aff94a42671fbc2a50f013b33953b0978
e-barr <31676765+e-barr@users.noreply.github.com>
2019-01-30 00:45:04 -0600

    add x button to close current image

--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -5,12 +5,16 @@ class ImageDisplay extends Component {
     render() {
         const { urls, description } = this.props.image
         return (
-            <div className="image-container">
-                <img 
-                    src={urls.regular} 
-                    alt={description} 
-                    onClick={this.props.closeImage}
-                 />
+            <div className="image-display">
+            <button onClick={this.props.closeImage}>X</button>
+                <div className="image-container">
+                    <a href={urls.regular} target="_blank" rel="noopener noreferrer">
+                        <img 
+                            src={urls.regular} 
+                            alt={description} 
+                        />
+                    </a>
+                </div>
             </div>
         )
     }
