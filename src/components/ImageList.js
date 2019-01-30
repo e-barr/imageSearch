3cea5e8d1266315da3618568e708e1df3145bdab
e-barr <31676765+e-barr@users.noreply.github.com>
2019-01-29 18:41:29 -0600

    add image card componenet

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard'
+
+// const ImageList = props => {
+//     const imagesFormatted = props.images.map(({ id, urls, description }) => <img key={id} src={urls.regular} alt={description} />)
+//     return <div className="image-list">{imagesFormatted}</div>
+// }
 
 const ImageList = props => {
-    const imagesFormatted = props.images.map(image => <img src={image.urls.regular} alt={image.description} />)
-    return <div>{imagesFormatted}</div>
+    const formattedImages = props.images.map((image) => <ImageCard image={image} key={image.id} />)
+    return <div className="image-list">{formattedImages}</div>
 }
-
 export default ImageList
